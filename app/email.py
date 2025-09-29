8fd2b42da71404e9bcbc85d69f15c78f5a6c7e95
robinxbaker <robinxbaker@gmail.com>
2025-09-29 18:00:04 -0500

    Made page look better

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
@@ -18,3 +19,12 @@ def send_password_reset_email(user):
                 html_body=render_template('email/reset_password.html',
                                           user=user, token=token))
     
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
